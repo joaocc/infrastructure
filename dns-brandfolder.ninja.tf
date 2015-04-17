a447c8eda7699c6355f3ece6eda42b1b823066c1
Jason Waldrip <jason@waldrip.net>
2015-04-16 23:09:33 -0600

    point to PaaS

--- dns-brandfolder.ninja.tf
+++ dns-brandfolder.ninja.tf
@@ -9,13 +9,13 @@ resource "aws_route53_record" "STAR-brandfolder-ninja" {
    name = "*.brandfolder.ninja"
    type = "CNAME"
    ttl = "300"
-   records = ["${aws_elb.www.dns_name}"]
+   records = ["${aws_elb.paas.dns_name}"]
 }
 
 resource "aws_route53_alias_target" "brandfolder-ninja" {
    zone_id = "${aws_route53_zone.brandfolder-ninja.zone_id}"
    name = "brandfolder.ninja"
    type = "A"
-   target = "${aws_elb.www.dns_name}"
-   target_zone_id = "${aws_elb.www.hosted_zone_id}"
+   target = "${aws_elb.paas.dns_name}"
+   target_zone_id = "${aws_elb.paas.hosted_zone_id}"
 }
