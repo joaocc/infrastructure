3bf09ac1db3e6b15082e9d8e3428ece04562fb57
Jason Waldrip <jason@waldrip.net>
2015-04-21 11:32:16 -0600

    add redis hosts

--- dns-brandfolder.host.tf
+++ dns-brandfolder.host.tf
@@ -29,3 +29,21 @@ resource "aws_route53_record" "prod-pg-brandfolder-host" {
   ttl = "300"
   records = ["${aws_db_instance.default.address}"]
 }
+
+# Redis Primary
+resource "aws_route53_record" "prod-1-redis-brandfolder-host" {
+  zone_id = "${aws_route53_zone.brandfolder-host.zone_id}"
+  name = "prod.1.redis.brandfolder.host"
+  type = "CNAME"
+  ttl = "300"
+  records = ["production-001.miqyjj.0001.use1.cache.amazonaws.com"]
+}
+
+# Redis Secondary
+resource "aws_route53_record" "prod-2-redis-brandfolder-host" {
+  zone_id = "${aws_route53_zone.brandfolder-host.zone_id}"
+  name = "prod.2.redis.brandfolder.host"
+  type = "CNAME"
+  ttl = "300"
+  records = ["production-002.miqyjj.0001.use1.cache.amazonaws.com"]
+}
