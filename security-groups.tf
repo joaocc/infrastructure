47c13ef0d30286ec8ca636d61582a8d76cecb964
Jason Waldrip <jason@waldrip.net>
2015-05-17 23:57:22 -0600

    allow elasticsearch into the private hosts

--- security-groups.tf
+++ security-groups.tf
@@ -150,6 +150,14 @@ resource "aws_security_group" "deis-private" {
     protocol = -1
     self = true
   }
+
+  # Allow inbound elasticsearch
+  ingress {
+      from_port = 9200
+      to_port = 9200
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 # Private DB
