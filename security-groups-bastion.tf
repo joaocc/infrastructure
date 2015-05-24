6ccfbc68c12e2dd224045869fc76a122de86c310
Jason Waldrip <jason@waldrip.net>
2015-05-24 12:06:01 -0600

    allow outbound connections from bastion

--- security-groups-bastion.tf
+++ security-groups-bastion.tf
@@ -12,6 +12,14 @@ resource "aws_security_group" "bastion" {
       cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Allow outbound
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   lifecycle { create_before_destroy = true }
 
 }
