b245c7e03cb739a886c6b144c2d1efa3a5ebe04d
Paul Arterburn <paul.arterburn@brandfolder.com>
2015-06-08 16:18:43 -0600

    add subdomains to bf-staging

--- dns-bf-staging.com.tf
+++ dns-bf-staging.com.tf
@@ -3,6 +3,15 @@ resource "aws_route53_zone" "bf-staging-com" {
    name = "bf-staging.com"
 }
 
+# Subdomains to Bf-staging.com
+resource "aws_route53_record" "STAR-bf-staging-com" {
+   zone_id = "${aws_route53_zone.bf-staging-com.zone_id}"
+   name = "*.bf-staging.com"
+   type = "CNAME"
+   ttl = 3600
+   records = ["${aws_elb.brandfolder-com.dns_name}"]
+}
+
 # Alias bf-staging.com
 resource "aws_route53_record" "A-bf-staging-com" {
    zone_id = "${aws_route53_zone.bf-staging-com.zone_id}"
