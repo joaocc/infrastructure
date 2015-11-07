c4eab94b3ff0ee9e13890828f07f6ca765fad4d5
Jason Waldrip <jason@waldrip.net>
2015-11-07 15:34:12 -0700

    Secondary Bastion

---
+++ host-bastion-2.tf
@@ -0,0 +1,28 @@
+# Bastion instance for remote ssh access
+resource "aws_instance" "bastion-2" {
+    ami = "${lookup(var.amis, "coreos_766_5_0")}"
+    key_name = "bastion-master"
+    instance_type = "${lookup(var.instance_types, "bastion")}"
+    subnet_id = "${aws_subnet.subnet.1.id}"
+    associate_public_ip_address = true
+    security_groups = ["${aws_security_group.bastion.id}"]
+    user_data = "${template_file.bastion.rendered}"
+
+    tags {
+        Name = "Bastion 2 (SSH Gateway)"
+        Type = "Bastion"
+    }
+
+    # Storage
+    root_block_device {
+      volume_size = 50
+      volume_type = "gp2"
+    }
+
+    ebs_block_device {
+      device_name = "/dev/xvdf"
+      volume_size = 100
+      volume_type = "gp2"
+    }
+
+}
