b212822505d7fb0d10468f741e509927016663a8
Jason Waldrip <jason@waldrip.net>
2016-02-03 17:18:45 -0700

    update timeouts

--- elb-brandfolder-all.tf
+++ elb-brandfolder-all.tf
@@ -9,7 +9,7 @@ resource "aws_elb" "brandfolder-all" {
     "${aws_security_group.internal-communication.id}"
   ]
   subnets = ["${aws_subnet.subnet.*.id}"]
-  idle_timeout = 1800
+  idle_timeout = 300
 
   # Listen for inbound HTTPS Connections
   listener {
